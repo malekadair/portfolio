bb3bf8713901d8aaa25fa7c31cec0a453652e4e7
Malek Haj-Hussein <malekadair@gmail.com>
2020-05-03 22:54:05 -0500

    added tournease and gottalotto projects

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+// $(() => {
+// 	$('header').on('mouseover', function (){
+// 		$('.lastName').show(1000);
+// 		$('.firstName').text('Malek');
+// 	})
+// 	$('header').on('mouseleave', function(){
+// 		$('.lastName').hide(1000);
+// 		$('.firstName').text('Malek.');
+// 	})
+// })
